b8c7da599a070c4e6e22e965c7b8f3f8235bfc99
liuxiaoyu-git <xiaoyliu@redhat.com>
2019-10-25 21:47:54 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<html>
+<div id="box">
+    <table width="100%" height="100%">
+        <tr>
+            <td align="center">
+                <image src="iphone-promotion1.png" />
+                <font size="16" color="#FF0000"><br>iPhoneX买一送一，快来剁手吧！</font>
+            </td>
+        </tr>
+    </table>
+</div>
+</html>
