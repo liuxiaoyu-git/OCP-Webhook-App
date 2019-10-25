5cf40e352b70ff02f64bcd2be383e57b75d490a4
liuxiaoyu-git <xiaoyliu@redhat.com>
2019-10-25 22:45:46 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,16 +1,12 @@
 <html>
     <head>
-        <?php
-            if ($currentLoop <= $totalTime)
-                echo "<meta http-equiv=\"refresh\" content=\"2\">";
-        ?>
+        <meta http-equiv=\"refresh\" content=\"2\">";
     </head>
     <body>
-        <div id="box">
-            <table width="70%" height="100%">
-                <tr>
-                    <td align="center"><image src="iphone-promotion1.png" /><font size="10" color="#FF0000"><br>iPhoneX买一送一</font></td>
-                </tr>
-            </table>
+        <table align="center">
+            <tr>
+                <td align="center"><image src="iphone-promotion1.png" /><font size="10" color="#FF0000"><br>iPhoneX买一送一</font></td>
+            </tr>
+        </table>
     </div>
 </html>
