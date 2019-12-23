01631f57ef8eec8aeef43c05fc46bd07687c1a3c
liuxiaoyu-git <xiaoyliu@redhat.com>
2019-12-23 16:29:25 +0800

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <body>
         <table align="center">
             <tr>
-                <td align="center"><image src="iphone-promotion2.png" /><font size="10" color="#FF0000"><br>iPhoneX买一送一</font></td>
+                <td align="center"><image src="iphone-promotion1.png" /><font size="10" color="#FF0000"><br>iPhoneX买一送一</font></td>
             </tr>
         </table>
     </div>
